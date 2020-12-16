7d9e15c3196954d072ba40f0f95a9b56f3df6551
陈泽洲 <georgechenxm@icloud.com>
2020-12-15 16:05:13 -0800

    preprocess large dataset

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,7 @@ import pandas as pd
 from genderize import Genderize
 
 # Example of loading data
-path = "./iclr2017_papers.csv"
+path = "./iclr2019_papers.csv"
 df = pd.read_csv(path)
 
 # authors -> first author
@@ -21,18 +21,23 @@ df_pp_2.loc[:,'first_name'] = df_pp_2['second_author'].str.split().str[0]
 df_pp_2['last_name'] = df_pp_2['second_author'].str.split().str[1]
 
 frames = [df_pp_1, df_pp_2]
-df_pp = pd.concat(frames)
+df_pp = pd.concat(frames, ignore_index=True)
 df_pp.drop('first_author', inplace=True, axis=1)
 df_pp.drop('second_author', inplace=True, axis=1)
 
 df = df_pp[['paper_id','first_name', 'last_name', 'decision']]
+
+# last name column -> race
+df = pred_wiki_ln(df, 'last_name')
+df['race'] = df['race'].apply(lambda x : str(x).split(',')[-1])
+
 
 # first name column -> first name list -> genderize -> gender list -> gender column
 first_name_list = list(df['first_name'])
 
 genders = Genderize(
     user_agent='GenderizeDocs/0.0',
-    api_key='c2a7acf70ef5f74435f0bcf4d075a5e4',
+    api_key='83a79cbb58ff2af93ced58f65482f6c9',
     timeout=5.0
 ).get(first_name_list)
 
@@ -40,13 +45,8 @@ genders = Genderize(
 gender_df = pd.DataFrame(genders)
 df['gender'] = gender_df['gender']
 
-print(df)
-
-# last name column -> race
-df = pred_wiki_ln(df, 'last_name')
-df['race'] = df['race'].apply(lambda x : str(x).split(',')[-1])
 
 # save to csv
 df['decision'] = df['decision'].apply(lambda x : str(x).split(' ')[0])
 df = df[['paper_id', 'gender', 'race', 'decision']]
-df.to_csv('iclr2017_papers_preprocessed.csv', index=False)
+df.to_csv('iclr2019_papers_preprocessed.csv', index=False)
